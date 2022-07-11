84d2900e0044d51210ae04c9f0ea9c4596c17c38
Asaduzzaman <ajaman375@gmail.com>
2022-07-11 10:00:22 -0700

    categories design and dummy category add

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,14 @@
 import React, {Component, Fragment} from 'react';
 import {Container} from "react-bootstrap";
 import FeaturedProducts from "../components/home/FeaturedProducts";
+import Categories from "../components/home/Categories";
 
 class HomePage extends Component {
     render() {
         return (
             <Fragment>
-
                     <FeaturedProducts/>
-
-
-
+                    <Categories/>
             </Fragment>
         );
     }
