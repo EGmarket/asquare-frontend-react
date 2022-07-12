1aa1ac746a0c57466f5bd3cf30d1cacca87ead73
Asaduzzaman <ajaman375@gmail.com>
2022-07-12 04:24:45 -0700

    mega menu is clickable

--- src/components/home/HomeTop.jsx
+++ src/components/home/HomeTop.jsx
@@ -7,7 +7,7 @@ class HomeTop extends Component {
     render() {
         return (
             <Fragment>
-                <Container>
+                <Container className="p-0 m-0 overflow-hidden" fluid={true}>
                     <Row>
                       <Col lg={3} md={3} sm={12}>
                           <MegaMenu/>
