88bdf468b82406e1531c2d116a1ce5335100b508
Asaduzzaman <ajaman375@gmail.com>
2022-07-11 22:17:51 -0700

    new Arrival is working with dummy product

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,16 @@ import React, {Component, Fragment} from 'react';
 import {Container} from "react-bootstrap";
 import FeaturedProducts from "../components/home/FeaturedProducts";
 import Categories from "../components/home/Categories";
+import Collection from "../components/home/Collection";
+import NewArrival from "../components/home/NewArrival";
 
 class HomePage extends Component {
     render() {
         return (
             <Fragment>
+                <NewArrival/>
                     <FeaturedProducts/>
+                     <Collection/>
                     <Categories/>
             </Fragment>
         );
