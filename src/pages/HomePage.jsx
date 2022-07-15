cc5ee2052a7ce0be7cd5ba21ddf2adb23fa65a71
Asaduzzaman <ajaman375@gmail.com>
2022-07-15 07:22:13 -0700

    making responsive

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,14 +7,23 @@ import NewArrival from "../components/home/NewArrival";
 import HomeTop from "../components/home/HomeTop";
 import NavMenuDesktop from "../components/common/NavMenuDesktop";
 import NavMenuMobile from "../components/common/NavMenuMobile";
+import HomeTopMobile from "../components/home/HomeTopMobile";
 
 class HomePage extends Component {
     render() {
         return (
             <Fragment>
-                {/*<NavMenuDesktop/>*/}
-                <NavMenuMobile/>
-                <HomeTop/>
+
+                <div className="Desktop">
+                    <NavMenuDesktop/>
+                    <HomeTop/>
+                </div>
+
+                <div className="Mobile">
+                    <NavMenuMobile/>
+                    <HomeTopMobile/>
+                </div>
+
                 <NewArrival/>
                 <FeaturedProducts/>
                 <Collection/>
